8644fdae1e67ba46db11f65bc705b5a699247465
Sergey V. Kosolapov <sergey.kosolapov@ssa-outsourcing.com>
2018-07-16 13:41:40 +0300

    removed old files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {TestComponent} from './test.component';
-import { CourseComponent } from './course/course.component';
-import {TestService} from './test.service';
-import { AuthorsComponent } from './authors/authors.component';
-import {AuthorsService} from './authors.service';
-import { CoursesComponent } from './courses/courses.component';
-import {SummaryPipe} from './summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    CourseComponent,
-    AuthorsComponent,
-    CoursesComponent,
-    SummaryPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [
-    TestService,
-    AuthorsService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
