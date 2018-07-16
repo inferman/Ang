96dfab0a0d5f1c3072898acfceda04e4344a3d7c
Sergey V. Kosolapov <sergey.kosolapov@ssa-outsourcing.com>
2018-07-16 10:31:09 +0300

    one more example

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course',
+  selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
-  constructor() { }
-
+  title = 'Some image';
+  imgUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  tableText = 'sadfasdf dsafsadfsd sdfsadfsdsdfsdaf sdfsadf';
   ngOnInit() {
   }
-
 }
