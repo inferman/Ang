c39c88dcbb9478081e251d8b14ba55dcf28c342c
Sergey <infernal.manowar@gmail.com>
2018-07-22 19:37:37 +0300

    router params

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Router, ParamMap, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,7 +8,16 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    });
+  }
 
   ngOnInit() {
     const snapId = this.route.snapshot.paramMap.get('id');
